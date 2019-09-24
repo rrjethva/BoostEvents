0b031a788a72d9fedf5f085b24560c2f3e32c512
Ravi Jethva <rrjethva@gmail.com>
2019-09-24 19:21:44 -0400

    updated eventbrite api location

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -17,18 +17,20 @@ console.log("HEY");
 
 
 var request = new XMLHttpRequest();
+var location = $(this).attr("locationInput");
 
-request.open('GET', 'https://private-anon-b47911321b-eventbriteapiv3public.apiary-proxy.com/v3/events/search/');
+request.open('GET', 'https://www.eventbriteapi.com/v3/events/search?location.address=' + location + '&expand=venue');
 
 request.setRequestHeader('Authorization', 'Bearer HO5AZTOREHNL2RLDBLQ4  ');
 request.setRequestHeader('Content-Type', 'application/json');
 
 request.onreadystatechange = function () {
   if (this.readyState === 4) {
-    console.log('Status:', this.status);
+    console.log(this.location);
+    console.log('location.address');
     console.log('Headers:', this.getAllResponseHeaders());
     console.log('Body:', this.responseText);
-    console.log(this.pagination.events);
+
     $('#body').text("<p>" + this.responseText + "</p>");
   }
 };
