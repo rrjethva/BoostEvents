de6d8e13dc3ae81fbf3b55187bd93e8c1dbbcc73
Valon Rama <vrama21@gmail.com>
2019-09-28 10:26:21 -0400

    Minor html and css cleanups. Also added a check to see if the logoURL exists and to append that image if it does.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,20 +1,20 @@
 let date;
 let vibe;
+let loc;
 let results = [];
-let city;
 
 $(".submit-btn").on("click", function (event) {
     event.preventDefault();
 
-    if ($("#locationInput").val() === "" || $("#date").val() === "" || $("#vibeInput").val() === "") {
+    if ($("#location-input").val() === "" || $("#date-input").val() === "" || $("#vibe-input").val() === "") {
         alert("Please fill in all required fields")
     } else {
-        date = moment($("#date").val().trim()).format("L");
-        vibe = $("#vibeInput").val().trim();
-        city = $("#locationInput").val().trim();
+        date = moment($("#date-input").val().trim()).format("L");
+        vibe = $("#vibe-input").val().trim();
+        loc = $("#location-input").val().trim();
 
-        // weatherAPI(city);
-        eventBriteAPI(vibe, city);
+        weatherAPI(loc);
+        eventBriteAPI(vibe, loc);
 
         setTimeout(function () {
             for (let i = 0; i < results.length; i++) {
@@ -22,23 +22,17 @@ $(".submit-btn").on("click", function (event) {
             }
         }, 100);
 
-
-        $("#date").val("");
-        $("#vibeInput").val("");
-        $("#locationInput").val("");
-        $(".results").empty();
+        $("#date-input").val("");
+        $("#vibe-input").val("");
+        $("#location-input").val("");
+        $("#accordion-parent").empty();
         results = [];
     }
 });
 
-// $(".btn-link").on("click", function () {
-//     targetID = $(this).attr("data-toggle");
-//     $(targetID).attr("class", "collapse hide");
-// });
-
-const weatherAPI = (city) => {
+const weatherAPI = (loc) => {
     const apiKey = "49a5dfb8d316b444e3e39062f4aa7fdf"
-    let q = city
+    let q = loc
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&units=imperial&appid=" + apiKey;
 
     $.ajax({
@@ -47,15 +41,15 @@ const weatherAPI = (city) => {
     }).then(function (response) {
         console.log(response)
         console.log(q);
-        
+
         var currentTemp = response.main.temp;
-        console.log("Current temp: " + currentTemp +"°F");
+        console.log("Current temp: " + currentTemp + "°F");
     });
 };
 
-const eventBriteAPI = (query, city) => {
+const eventBriteAPI = (query, loc) => {
     let q = query
-    let locationAddress = city;
+    let locationAddress = loc;
 
     let eventURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + q + "&location.address=" + locationAddress + "&location.within=10km";
 
@@ -73,15 +67,25 @@ const eventBriteAPI = (query, city) => {
         console.log(response)
         for (let i = 0; i < 10; i++) {
             event = response.events[i];
+            let eventObj = {};
 
-            let eventObj = {
-                name: event.name.text,
-                description: event.description.text,
-                startTime: event.start.local,
-                endTime: event.end.local,
-                url: event.url,
-                // TODO: Check if logo is null
-                // logoURL: event.logo.url
+            if (event.logo !== null) {
+                eventObj = {
+                    name: event.name.text,
+                    description: event.description.text,
+                    startTime: event.start.local,
+                    endTime: event.end.local,
+                    url: event.url,
+                    logoURL: event.logo.url
+                }
+            } else {
+                eventObj = {
+                    name: event.name.text,
+                    description: event.description.text,
+                    startTime: event.start.local,
+                    endTime: event.end.local,
+                    url: event.url
+                }
             };
             results.push(eventObj);
         }
@@ -103,7 +107,7 @@ const createEventCard = (event, index) => {
     cardHeadButton.attr("aria-controls", "event-" + index);
     cardHead.append(cardHeadButton);
 
-    let cardBodyWrapper = $("<div>").addClass("collapse show");
+    let cardBodyWrapper = $("<div>").addClass("collapse hide");
     cardBodyWrapper.attr("id", "event-" + index);
     cardBodyWrapper.attr("aria-labelledby", "header-" + index);
     cardBodyWrapper.attr("data-parent", "#accordion-parent");
@@ -111,13 +115,18 @@ const createEventCard = (event, index) => {
     let cardBody = $("<div>").addClass("card-body");
     let cardTitle = $("<h5>").addClass("card-title").text(moment(event.startTime).format("lll") + " to " + moment(event.endTime).format("lll"));
     let cardText = $("<p>").addClass("card-text").text(event.description);
+    
+    if (event.logoURL) {
+        let cardImg = $("<img>").attr("src", event.logoURL);
+        cardBody.append(cardImg);
+    }
+        
     let link = $("<a>").addClass("btn btn-primary").text("Go ->");
     link.attr("href", event.url);
     link.attr("target", "_blank");
 
     cardBody.append(cardTitle, cardText, link);
     cardBodyWrapper.append(cardBody);
-
     card.append(cardHead, cardBodyWrapper);
     $("#accordion-parent").append(card);
 }
