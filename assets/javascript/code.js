69684bed754c1543b4707f3bd405bde653e68ac7
Kevin Perez <kevinperez996@outlook.com>
2019-10-02 21:10:16 -0400

    change CSS a bit

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,43 +1,18 @@
+$(document).ready(function() {
+
 let date;
 let vibe;
 let loc;
-<<<<<<< HEAD
-
-$(".submit-btn").on("click", function (event) {
-    event.preventDefault();
-
-    if ($("#location-input").val() === "" || $("#date-input").val() === "" || $("#vibe-input").val() === "") {
-        alert("Please fill in all required fields")
-    } else {
-        date = moment($("#date-input").val().trim()).format("YYYY-MM-DDThh:mm:ss");
-        vibe = $("#vibe-input").val().trim();
-        loc = $("#location-input").val().trim();
-
-        eventBriteSearchAPI(vibe, loc, date);
-
-        $("#date-input").val("");
-        $("#vibe-input").val("");
-        $("#location-input").val("");
-        $("#accordion-parent").empty();
-        results = [];
-    }
-});
-
-const eventBriteSearchAPI = (query, loc, date) => {
-    let results = [];
-=======
 // let completeResults = [];
 let eventVenueResults = [];
 let eventSearchResults = [];
 
 const eventBriteSearchAPI = async (query, loc, date) => {
->>>>>>> 75a87829563aefe74333121e36d587c2505b084f
     let eventSearchURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + query +
         "&location.address=" + loc +
         "&location.within=10km" +
         "&start_date.range_start=" + date +
         "&sort_by=best";
-
 
     const eventSearchSettings = {
         url: eventSearchURL,
@@ -52,46 +27,28 @@ const eventBriteSearchAPI = async (query, loc, date) => {
         console.log(response)
 
         for (let i = 0; i < 10; i++) {
-            let eventObj = {};
-            event = response.events[i];
+            let eventSearchObj = {};
+            const event = response.events[i];
 
             if (event.logo !== null) {
-                eventObj = {
+                eventSearchObj = {
                     name: event.name.text,
                     description: event.description.text,
                     startTime: event.start.local,
                     endTime: event.end.local,
                     url: event.url,
                     logoURL: event.logo.url,
-                    venueID: event.venue_id
+                    venueId: event.venue_id,
                 }
             } else {
-                eventObj = {
+                eventSearchObj = {
                     name: event.name.text,
                     description: event.description.text,
                     startTime: event.start.local,
                     endTime: event.end.local,
                     url: event.url,
-                    venueID: event.venue_id
+                    venueId: event.venue_id,
                 }
-<<<<<<< HEAD
-            }
-
-            eventBriteVenueAPI(event.venue_id);
-
-            results.push(eventObj);
-        };
-        // TODO: Sort results data by the date
-
-        for (let i = 0; i < results.length; i++) {
-            createEventCard(results[i], i);
-        };
-    });
-}
-
-const eventBriteVenueAPI = (venue_id) => {
-
-=======
             };
             eventSearchResults.push(eventSearchObj);
         };
@@ -102,23 +59,16 @@ const eventBriteVenueAPI = (venue_id) => {
 }
 
 const eventBriteVenueAPI = async (venue_id) => {
->>>>>>> 75a87829563aefe74333121e36d587c2505b084f
     let eventVenueURL = "https://www.eventbriteapi.com/v3/venues/" + venue_id + "/";
 
     const eventVenueSettings = {
         url: eventVenueURL,
         method: "GET",
         headers: {
-            "Authorization": "Bearer IBJWZWCSCKANXCVUEZAY",
-        },
+            "Authorization": "Bearer IBJWZWCSCKANXCVUEZAY"
+        }
     };
 
-<<<<<<< HEAD
-    $.ajax(eventVenueSettings).then(function (response) {
-        console.log(response);
-    })
-}
-=======
     try {
         const response = await $.ajax(eventVenueSettings)
 
@@ -140,7 +90,6 @@ const eventBriteVenueAPI = async (venue_id) => {
         console.log(error);
     }
 };
->>>>>>> 75a87829563aefe74333121e36d587c2505b084f
 
 
 
@@ -171,7 +120,7 @@ const createEventCard = (event, eventIndex) => {
         "data-parent": "#accordion-parent"
     });
 
-    let cardBody = $("<div>").addClass("card-body");
+    let cardBody = $("<div>").addClass("card-body row");
     let cardTitle = $("<h5>").addClass("card-title").text(moment(event.startTime).format("lll") + " to " + moment(event.endTime).format("LT"));
     let cardText = $("<p>").addClass("card-text").text(event.description);
 
@@ -181,6 +130,11 @@ const createEventCard = (event, eventIndex) => {
         cardBody.append(cardImg);
     }
 
+    let cardMap = $("<div>").attr({
+        "class": "map",
+        "id": "map-" + eventIndex
+    });
+
     let link = $("<a>").text("Go to Page");
     link.attr({
         "class": "btn btn-primary",
@@ -188,13 +142,6 @@ const createEventCard = (event, eventIndex) => {
         "target": "_blank"
     });
 
-<<<<<<< HEAD
-    cardBody.append(cardTitle, cardText, link);
-    cardBodyWrapper.append(cardBody);
-    card.append(cardHead, cardBodyWrapper);
-    $("#accordion-parent").append(card);
-}
-=======
     cardBody.append(cardTitle, cardText, cardMap, link);
     cardBodyWrapper.append(cardBody);
     card.append(cardHead, cardBodyWrapper);
@@ -241,4 +188,4 @@ const createGoogleMapsScript = () => {
     let googleMapsScript = $("<script>").attr("src", "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXYZDR89jXI_Ml7MCaV4TskRBqkkeZ7hk&callback=createMapMarker")
     $("body").append(googleMapsScript);
 }
->>>>>>> 75a87829563aefe74333121e36d587c2505b084f
+});
